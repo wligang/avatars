e66487ce4b82cfc89a7716f761d7db2ce9c55130
Ligang.Wang <wlgchun@163.com>
2019-04-23 18:45:57 +0800

    add mybaties files

--- avatar-service/src/main/java/com/wlgdo/avatar/service/AvatarServiceApplication.java
+++ avatar-service/src/main/java/com/wlgdo/avatar/service/AvatarServiceApplication.java
@@ -1,9 +1,11 @@
 package com.wlgdo.avatar.service;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.wlgdo.avatar.service.dao")
 public class AvatarServiceApplication {
 
     public static void main(String[] args) {
---
+++ avatar-service/src/main/java/com/wlgdo/avatar/service/dao/UserMapper.java
@@ -0,0 +1,4 @@
+package com.wlgdo.avatar.service.dao;
+
+public interface UserMapper {
+}
