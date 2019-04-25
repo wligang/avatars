7fb8d35ec932c94a4acdc1cff13b00b8fc1feba2
Ligang.Wang <wlgchun@163.com>
2019-04-26 01:02:39 +0800

    update:provider complet

--- avatar-service/src/main/java/com/wlgdo/avatar/service/AvatarServiceApplication.java
+++ avatar-service/src/main/java/com/wlgdo/avatar/service/AvatarServiceApplication.java
@@ -8,10 +8,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@MapperScan("com.wlgdo.avatar.service.dao")
+
 @SpringBootApplication
 // 开启dubbo的自动配置
 @EnableDubboConfiguration
+@MapperScan("com.wlgdo.avatar.service.dao")
 public class AvatarServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(AvatarServiceApplication.class, args);
--- avatar-service/src/main/java/com/wlgdo/avatar/service/users/UserServiceImpl.java
+++ avatar-service/src/main/java/com/wlgdo/avatar/service/users/UserServiceImpl.java
@@ -4,8 +4,11 @@ package com.wlgdo.avatar.service.users;
 import com.alibaba.dubbo.config.annotation.Service;
 import com.wlgdo.avatar.api.service.IUserService;
 
+import com.wlgdo.avatar.service.dao.UserMapper;
+import com.wlgdo.avatar.service.model.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -14,13 +17,13 @@ public class UserServiceImpl implements IUserService {
 
     Logger logger = LoggerFactory.getLogger(getClass());
 
-//    @Autowired
-//    private UserMapper userMapper;
+    @Autowired
+    private UserMapper userMapper;
 
     @Override
     public String getUserName() {
-//        User user = userMapper.findUser();
-//        logger.info("user:{}", user);
-        return "My name is feify";
+        User user = userMapper.findUser();
+        logger.info("user:{}", user);
+        return user == null ? "My name is feify" : user.getName();
     }
 }
