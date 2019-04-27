fcc00e640ad67f8f8f65d6ce9e00aa0d158b188f
Ligang.Wang <wlgchun@163.com>
2019-04-27 20:50:03 +0800

    update:替换admin项目

---
+++ avatar-admin-server/src/main/java/com/wlgdo/avatar/admin/controller/LoginController.java
@@ -0,0 +1,48 @@
+package com.wlgdo.avatar.admin.controller;
+
+import com.alibaba.fastjson.JSONObject;
+import com.wlgdo.avatar.admin.service.LoginService;
+import com.wlgdo.avatar.admin.utils.CommonUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author: Ligang.Wang[wlgchun@163.com]
+ * @date: 2019/04/26
+ * @description: 登录相关Controller
+ */
+@RestController
+@RequestMapping("/login")
+public class LoginController {
+
+	@Autowired
+	private LoginService loginService;
+
+	/**
+	 * 登录
+	 */
+	@PostMapping("/auth")
+	public JSONObject authLogin(@RequestBody JSONObject requestJson) {
+		CommonUtil.hasAllRequired(requestJson, "username,password");
+		return loginService.authLogin(requestJson);
+	}
+
+	/**
+	 * 查询当前登录用户的信息
+	 */
+	@PostMapping("/getInfo")
+	public JSONObject getInfo() {
+		return loginService.getInfo();
+	}
+
+	/**
+	 * 登出
+	 */
+	@PostMapping("/logout")
+	public JSONObject logout() {
+		return loginService.logout();
+	}
+}
