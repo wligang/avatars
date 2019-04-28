e6f400d469bd76ec57cef0e91b43f52bb8c8dcca
Ligang.Wang <wlgchun@163.com>
2019-04-28 21:47:27 +0800

    update:update github target url to myself

--- avatar-admin-web/src/views/dashboard/index.vue
+++ avatar-admin-web/src/views/dashboard/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="dashboard-container">
-    <a href="https://github.com/Heeexy/SpringBoot-Shiro-Vue" target="_blank"><img style="margin: 40px"
+    <a href="https://github.com/wligang/avatars" target="_blank"><img style="margin: 40px"
       src="http://img.heeexy.com/github.png"></a>
   </div>
 </template>
