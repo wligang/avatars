4d0daec501d850c18c004fa099a1fd44d9c0f6d0
Ligang.Wang <wlgchun@163.com>
2019-05-02 13:13:55 +0800

    add:add python project for data

---
+++ avatar-data/avatar/blog.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Ligang.Wang
+@license: (C) Copyright 2018-2022, wlgdo.com Manager Corporation Limited.
+@contact: wlgchun@163.com
+@file: blog.py
+@time: 2019/5/1 21:56
+@desc:
+'''
---
+++ avatar-data/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: Ligang.Wang
+@license: (C) Copyright 2018-2022, wlgdo.com Manager Corporation Limited.
+@contact: wlgchun@163.com
+@file: setup.py
+@time: 2019/4/30 22:01
+@desc:
+'''
+
+from setuptools import setup, find_packages
+setup(
+    name = "wlgdo",
+    version = "0.1",
+    packages = find_packages(),
+)
