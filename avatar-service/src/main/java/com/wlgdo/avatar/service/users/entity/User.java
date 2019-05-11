41008f6cfaeb8781a5e7cd9da60cf22c241f77ef
Ligang.Wang <wlgchun@163.com>
2019-05-11 14:36:27 +0800

    update:change framework template to velocity

--- avatar-service/src/main/java/com/wlgdo/avatar/service/users/entity/User.java
+++ avatar-service/src/main/java/com/wlgdo/avatar/service/users/entity/User.java
@@ -1,11 +1,12 @@
 package com.wlgdo.avatar.service.users.entity;
 
 import com.baomidou.mybatisplus.annotation.TableField;
+import lombok.Data;
 
 import java.io.Serializable;
 import java.util.Date;
 
-
+@Data
 public class User implements Serializable {
 
     private Integer id;
@@ -20,45 +21,6 @@ public class User implements Serializable {
     private Date updateTime;
 
 
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getAccountNo() {
-        return accountNo;
-    }
-
-    public void setAccountNo(String accountNo) {
-        this.accountNo = accountNo;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
 
     @Override
     public String toString() {
