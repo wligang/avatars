4aec663c9e7324aaea3f8d29861e2445303410d2
Ligang.Wang <wlgchun@163.com>
2019-05-15 00:15:47 +0800

    add:添加初始版本的actuator和springbootAdmin

---
+++ avatar-web-monitor/src/main/java/com/wlgdo/avatar/web/AvatarMonitorServerApplication.java
@@ -0,0 +1,17 @@
+package com.wlgdo.avatar.web;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class AvatarMonitorServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvatarMonitorServerApplication.class, args);
+    }
+
+}
