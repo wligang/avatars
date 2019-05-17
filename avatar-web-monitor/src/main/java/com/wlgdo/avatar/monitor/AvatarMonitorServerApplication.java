a3cde91caea5f62f4e8acaa92407368544cb0fd8
Ligang.Wang <wlgchun@163.com>
2019-05-17 19:09:37 +0800

    update:make project code perfect

--- avatar-web-monitor/src/main/java/com/wlgdo/avatar/monitor/AvatarMonitorServerApplication.java
+++ avatar-web-monitor/src/main/java/com/wlgdo/avatar/monitor/AvatarMonitorServerApplication.java
@@ -3,10 +3,10 @@ package com.wlgdo.avatar.monitor;
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
-@EnableAutoConfiguration
+@SpringBootApplication
 @EnableAdminServer
 public class AvatarMonitorServerApplication {
 
