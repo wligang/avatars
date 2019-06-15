3a49fa02340961b8c4c40f7b5de567f82e5af38e
Ligang.Wang <wlgchun@163.com>
2019-06-16 00:00:02 +0800

    update:code formate

---
+++ avatar-common/src/main/java/com/wlgdo/avatar/common/utils/BeanMapper.java
@@ -0,0 +1,14 @@
+package com.wlgdo.avatar.common.utils;
+
+/**
+ * Author: Ligang.Wang[wlgchun@l63.com]
+ * Date: 2019/6/15 23:57
+ */
+public class BeanMapper {
+
+    public Object map(Object source, Object target) {
+        return null;
+    }
+
+
+}
