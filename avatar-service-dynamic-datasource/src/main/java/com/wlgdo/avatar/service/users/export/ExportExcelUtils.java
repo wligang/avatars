89e0b52e5339122ebecf31976fc26d25064815ef
Ligang.Wang <wlgchun@163.com>
2019-06-25 23:54:27 +0800

    update:添加用户到处捣乱Excel表格的功能

--- avatar-service-dynamic-datasource/src/main/java/com/wlgdo/avatar/service/users/export/ExportExcelUtils.java
+++ avatar-service-dynamic-datasource/src/main/java/com/wlgdo/avatar/service/users/export/ExportExcelUtils.java
@@ -64,10 +64,10 @@ public class ExportExcelUtils {
         titleFont.setColor(IndexedColors.BLACK.index);
 
         XSSFCellStyle titleStyle = wb.createCellStyle();
-        titleStyle.setAlignment(XSSFCellStyle.ALIGN_CENTER);
-        titleStyle.setVerticalAlignment(XSSFCellStyle.VERTICAL_CENTER);
+//        titleStyle.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+//        titleStyle.setVerticalAlignment(XSSFCellStyle.VERTICAL_CENTER);
         titleStyle.setFillForegroundColor(new XSSFColor(new Color(182, 184, 192)));
-        titleStyle.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
+//        titleStyle.setFillPattern(XSSFCellStyle.SOLID_FOREGROUND);
         titleStyle.setFont(titleFont);
         setBorder(titleStyle, BorderStyle.THIN, new XSSFColor(new Color(0, 0, 0)));
 
@@ -95,8 +95,8 @@ public class ExportExcelUtils {
         dataFont.setColor(IndexedColors.BLACK.index);
 
         XSSFCellStyle dataStyle = wb.createCellStyle();
-        dataStyle.setAlignment(XSSFCellStyle.ALIGN_CENTER);
-        dataStyle.setVerticalAlignment(XSSFCellStyle.VERTICAL_CENTER);
+//        dataStyle.setAlignment(XSSFCellStyle.ALIGN_CENTER);
+//        dataStyle.setVerticalAlignment(XSSFCellStyle.VERTICAL_CENTER);
         dataStyle.setFont(dataFont);
         setBorder(dataStyle, BorderStyle.THIN, new XSSFColor(new Color(0, 0, 0)));
 
