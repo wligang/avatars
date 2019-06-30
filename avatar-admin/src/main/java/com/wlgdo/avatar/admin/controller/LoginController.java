13cf0f7b3d9ff190830671b3c465c6d6c1227e62
Ligang.Wang <wlgchun@163.com>
2019-06-30 23:58:00 +0800

    update

--- avatar-admin/src/main/java/com/wlgdo/avatar/admin/controller/LoginController.java
+++ avatar-admin/src/main/java/com/wlgdo/avatar/admin/controller/LoginController.java
@@ -3,6 +3,8 @@ package com.wlgdo.avatar.admin.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.wlgdo.avatar.admin.service.LoginService;
 import com.wlgdo.avatar.admin.utils.CommonUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,31 +20,35 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/login")
 public class LoginController {
 
-	@Autowired
-	private LoginService loginService;
+    Logger logger = LoggerFactory.getLogger(getClass());
 
-	/**
-	 * 登录
-	 */
-	@PostMapping("/auth")
-	public JSONObject authLogin(@RequestBody JSONObject requestJson) {
-		CommonUtil.hasAllRequired(requestJson, "username,password");
-		return loginService.authLogin(requestJson);
-	}
+    @Autowired
+    private LoginService loginService;
 
-	/**
-	 * 查询当前登录用户的信息
-	 */
-	@PostMapping("/getInfo")
-	public JSONObject getInfo() {
-		return loginService.getInfo();
-	}
+    /**
+     * 登录
+     */
+    @PostMapping("/auth")
+    public JSONObject authLogin(@RequestBody JSONObject requestJson) {
 
-	/**
-	 * 登出
-	 */
-	@PostMapping("/logout")
-	public JSONObject logout() {
-		return loginService.logout();
-	}
+        CommonUtil.hasAllRequired(requestJson, "username,password");
+        logger.info("用户开始登录:{}", requestJson);
+        return loginService.authLogin(requestJson);
+    }
+
+    /**
+     * 查询当前登录用户的信息
+     */
+    @PostMapping("/getInfo")
+    public JSONObject getInfo() {
+        return loginService.getInfo();
+    }
+
+    /**
+     * 登出
+     */
+    @PostMapping("/logout")
+    public JSONObject logout() {
+        return loginService.logout();
+    }
 }
