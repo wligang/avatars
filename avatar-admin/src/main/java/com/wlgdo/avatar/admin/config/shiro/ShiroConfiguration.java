8b18d3560116f3d96ac221c05eb51772a4d023b9
Ligang.Wang <wlgchun@163.com>
2019-06-30 20:44:44 +0800

    optimization:启动项目后，可以多策略制定任务调度的触发

--- avatar-admin/src/main/java/com/wlgdo/avatar/admin/config/shiro/ShiroConfiguration.java
+++ avatar-admin/src/main/java/com/wlgdo/avatar/admin/config/shiro/ShiroConfiguration.java
@@ -41,6 +41,7 @@ public class ShiroConfiguration {
 		Map<String, String> filterChainDefinitionMap = new LinkedHashMap<>();
          /* 过滤链定义，从上向下顺序执行，一般将 / ** 放在最为下边:这是一个坑呢，一不小心代码就不好使了;
           authc:所有url都必须认证通过才可以访问; anon:所有url都都可以匿名访问 */
+
 		filterChainDefinitionMap.put("/", "anon");
 		filterChainDefinitionMap.put("/static/**", "anon");
 		filterChainDefinitionMap.put("/login/auth", "anon");
