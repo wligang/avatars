c427e45c260aade9948c789bc6c0b305b7ff650b
Ligang.Wang <wangligang@eglsgame.com>
2019-08-05 21:11:37 +0800

    drop error configuraction

--- avatar-dynamic-quartz/src/main/java/com/wlgdo/avatar/quartz/AvatarDQApplication.java
+++ avatar-dynamic-quartz/src/main/java/com/wlgdo/avatar/quartz/AvatarDQApplication.java
@@ -9,8 +9,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import javax.annotation.PostConstruct;
-
 
 @SpringBootApplication
 @MapperScan("com.wlgdo.avatar.quartz.*.mapper")
@@ -26,11 +24,5 @@ public class AvatarDQApplication {
     @Bean
     public PaginationInterceptor paginationInterceptor() {
         return new PaginationInterceptor();
-    }
-
-
-    @PostConstruct
-    public void init() {
-        System.out.println("init................");
     }
 }
