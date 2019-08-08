1526e8c38e1362a1463b07da071c4170180aac3e
Ligang.Wang <wangligang@eglsgame.com>
2019-08-08 20:54:09 +0800

    It`s must a accident what i throw the main application

---
+++ avatar-admin/src/main/java/com/wlgdo/avatar/admin/AvatarAdminApplication.java
@@ -0,0 +1,9 @@
+package com.wlgdo.avatar.admin;
+
+/**
+ * @description: admin Application
+ * @author: Ligang.Wang[wangligang@eglagame.com]
+ * @create: 2019-08-08 20:49
+ **/
+public class AvatarAdminApplication {
+}
