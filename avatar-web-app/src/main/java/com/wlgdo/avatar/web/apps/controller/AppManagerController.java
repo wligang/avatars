b1befdc86154f47876c290327cb81195c2d94b43
Ligang.Wang <wangligang@eglsgame.com>
2019-08-08 20:09:37 +0800

    import slf4j annontate

--- avatar-web-app/src/main/java/com/wlgdo/avatar/web/apps/controller/AppManagerController.java
+++ avatar-web-app/src/main/java/com/wlgdo/avatar/web/apps/controller/AppManagerController.java
@@ -3,8 +3,7 @@ package com.wlgdo.avatar.web.apps.controller;
 import com.alibaba.fastjson.JSONObject;
 import com.wlgdo.avatar.dubbo.rpc.Resp;
 import com.wlgdo.avatar.dubbo.rpc.apps.AppInfo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,25 +13,26 @@ import javax.annotation.Resource;
  * Author: Ligang.Wang[wlgchun@l63.com]
  * Date: 2019/5/3 23:04
  */
+@Slf4j
 @RestController
 public class AppManagerController {
     private static final String APP_NAME_KEY = "appName_";
 
-    Logger logger = LoggerFactory.getLogger(getClass());
 
     @Resource
     private StringRedisTemplate stringRedisTemplate;
 
     @GetMapping("apps/info/{key}")
     public Object index(@PathVariable String key) {
+        log.info("获取应用信息:{}", key, key, key, key, key, key);
         JSONObject respData = new JSONObject();
         if (stringRedisTemplate.hasKey(APP_NAME_KEY + key)) {
-            JSONObject jsonObject = new JSONObject().parseObject(stringRedisTemplate.opsForValue().get(APP_NAME_KEY + key));
-            logger.info("userInfo:{}", jsonObject);
+            JSONObject jsonObject = JSONObject.parseObject(stringRedisTemplate.opsForValue().get(APP_NAME_KEY + key));
+            log.info("userInfo:{}", jsonObject);
             respData.put("appInfo", jsonObject);
         }
         if (stringRedisTemplate.hasKey("userInfo_LeegooWang")) {
-            JSONObject hotArticles = new JSONObject().parseObject(stringRedisTemplate.opsForValue().get("userInfo_LeegooWang"));
+            JSONObject hotArticles = JSONObject.parseObject(stringRedisTemplate.opsForValue().get("userInfo_LeegooWang"));
             respData.put("hotArticles", hotArticles.get("articles"));
         }
         return new Resp(respData);
@@ -40,7 +40,7 @@ public class AppManagerController {
 
     @PostMapping("apps/info")
     public Object save(@RequestBody AppInfo app) {
-        logger.info("保存app的设置：{}", app);
+        log.info("保存app的设置：{}", app);
         stringRedisTemplate.opsForValue().set(APP_NAME_KEY + app.getAppKey(), JSONObject.toJSONString(app));
         return new Resp(app);
     }
