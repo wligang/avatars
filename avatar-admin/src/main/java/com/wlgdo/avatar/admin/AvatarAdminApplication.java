9d1645192d9f8a92901c81ef2413913cc54defce
Ligang.Wang <wangligang@eglsgame.com>
2019-08-08 20:55:54 +0800

    exception#It`s maby a accident that i lost the main application

--- avatar-admin/src/main/java/com/wlgdo/avatar/admin/AvatarAdminApplication.java
+++ avatar-admin/src/main/java/com/wlgdo/avatar/admin/AvatarAdminApplication.java
@@ -1,9 +1,22 @@
 package com.wlgdo.avatar.admin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * @description: admin Application
  * @author: Ligang.Wang[wangligang@eglagame.com]
  * @create: 2019-08-08 20:49
  **/
+
+@SpringBootApplication
 public class AvatarAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication application = new SpringApplication(AvatarAdminApplication.class);
+        application.run(args);
+
+
+    }
+
 }
