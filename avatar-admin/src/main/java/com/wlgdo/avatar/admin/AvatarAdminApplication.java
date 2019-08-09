e5ac6dbee7106193b4d06e61034266d0768eb689
Ligang.Wang <wlgchun@163.com>
2019-08-10 00:56:26 +0800

    update:rebuid project struts

--- avatar-admin/src/main/java/com/wlgdo/avatar/admin/AvatarAdminApplication.java
+++ avatar-admin/src/main/java/com/wlgdo/avatar/admin/AvatarAdminApplication.java
@@ -1,22 +1,29 @@
 package com.wlgdo.avatar.admin;
-
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
- * @description: admin Application
- * @author: Ligang.Wang[wangligang@eglagame.com]
- * @create: 2019-08-08 20:49
- **/
-
+ * @author: Ligang.Wang[wlgchun@163.com]
+ * @description: SpringBoot启动类
+ * @date: 20190426
+ */
 @SpringBootApplication
-public class AvatarAdminApplication {
+@MapperScan("com.wlgdo.avatar.admin.dao")
+public class AvatarAdminApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(AvatarAdminApplication.class);
+        application.setBannerMode(Banner.Mode.OFF);
         application.run(args);
-
-
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        // 注意这里要指向原先用main方法执行的Application启动类
+        return builder.sources(AvatarAdminApplication.class);
+    }
 }
