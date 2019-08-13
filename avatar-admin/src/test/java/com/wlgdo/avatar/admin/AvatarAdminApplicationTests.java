fa5d5a73deb8bc192b21d583b3405209428fb17e
Ligang.Wang <wlgchun@163.com>
2019-08-13 23:46:55 +0800

    modify linguist-language

---
+++ avatar-admin/src/test/java/com/wlgdo/avatar/admin/AvatarAdminApplicationTests.java
@@ -0,0 +1,20 @@
+package com.wlgdo.avatar.admin;
+/**
+ * @author: Ligang.Wang[wlgchun@163.com]
+ * @date: 2019/04/26
+ */
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvatarAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
---
+++ avatar-web-monitor/src/test/java/com/wlgdo/avatar/monitor/AvatarWebApplicationTests.java
@@ -0,0 +1,16 @@
+package com.wlgdo.avatar.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AvatarWebApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
