f1f8794bda0d2bd0e9c4074001c76638c8eeafab
Ligang.Wang <wlgchun@163.com>
2019-08-19 00:50:17 +0800

    drop front project avatar-admin-web

--- avatar-web-app/src/main/java/com/wlgdo/avatar/web/apps/controller/AppOauthController.java
+++ avatar-web-app/src/main/java/com/wlgdo/avatar/web/apps/controller/AppOauthController.java
@@ -1,5 +1,7 @@
 package com.wlgdo.avatar.web.apps.controller;
 
+import cn.hutool.core.map.MapUtil;
+import cn.hutool.http.HttpUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.tomcat.util.security.MD5Encoder;
@@ -7,8 +9,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Map;
 
 /**
  * Author: Ligang.Wang[wlgchun@l63.com]
@@ -18,28 +23,51 @@ import javax.servlet.http.HttpServletResponse;
 @Controller
 public class AppOauthController {
 
-    private static String redirect_uri = "http://www.wlgdo.com/oauth";
-    private static String client_id = "189881679c4983dd7ae6";
-    private static String client_secret = "1fbf8e8f67933b28d075fc77af7c10a3a14d7f0d";
-    private static String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token?client_id=189881679c4983dd7ae6&client_secret=1fbf8e8f67933b28d075fc77af7c10a3a14d7f0d&redirect_uri=http://www.wlgdo.com/oauth&code=2f5ca04906e841b4e8e3&state=213844c9-bbd6-4016-9b1c-94776a24e3a8";
+    private static String redirect_uri = "http://www.wlgdo.com/oauth?";
+    private static String CLIENT_ID = "189881679c4983dd7ae6";
+    private static String CLIENT_SECRET = "1fbf8e8f67933b28d075fc77af7c10a3a14d7f0d";
+    private static String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token?";
 //    private static String ACCESS_TOKEN_URL = "https://github.com/login/oauth/access_token?client_id=189881679c4983dd7ae6&client_secret=1fbf8e8f67933b28d075fc77af7c10a3a14d7f0d&redirect_uri=http://www.wlgdo.com/oauth&code=2f5ca04906e841b4e8e3&state=213844c9-bbd6-4016-9b1c-94776a24e3a8";
 
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
 
-    @RequestMapping("oauth/github")
-    public Object callback(String code, String access_token, String scope, HttpServletResponse response) {
+    @RequestMapping("github")
+    public Object callback(@RequestParam(required = false) String code, @RequestParam(required = false) String access_token,
+                           @RequestParam(required = false) String scope,
+                           @RequestParam(required = false) String state, HttpServletResponse response) throws IOException {
 
         log.info("获取到的token是: [{} , {}]", code, scope);
-
+        String accessToken = null;
         if (StringUtils.isNotBlank(code)) {
+            StringBuffer url = new StringBuffer(ACCESS_TOKEN_URL)
+                    .append("client_id=").append(CLIENT_ID)
+                    .append("&client_secret=").append(CLIENT_SECRET)
+                    .append("&code=").append(code)
+                    .append("&state=").append(state);
+            String accessTokenStr = HttpUtil.get(url.toString());
 
-
-
+            log.info("获取到的授权是：{}", accessTokenStr);
+            accessToken = accessTokenStr.split("&")[0];
+            if (accessToken != null && accessToken.indexOf("access_token") > -1 && accessToken != null) {
+                stringRedisTemplate.opsForValue().set("access_token", accessToken);
+                String redirectUrl = redirect_uri + "?" + accessToken;
+                log.info("获取到的授权是：{}", redirectUrl);
+                response.sendRedirect(redirectUrl);
+            }
+            return "授权失败" + accessTokenStr;
+        }
+        if (StringUtils.isNotBlank(access_token)) {
+            stringRedisTemplate.opsForValue().set("access_token", accessToken);
+            String redirectUrl = redirect_uri + "?" + accessToken;
+            log.info("获取到的授权是：{}", redirectUrl);
+            response.sendRedirect(redirectUrl);
         }
 
-//        response.sendRedirect(redirect_uri + "?access_token=" + MD5Encoder.encode(userInfo.getPassword().getBytes("UTF-8")));
-        return "";
+        if (StringUtils.isNotBlank(access_token)) {
+            stringRedisTemplate.opsForValue().set("access_token", accessToken);
+        }
+        return String.format("授权失败[%s]", code, access_token);
     }
 
 }
