d4243d7a0cb7f3ef4865c2170393eba74aa845cb
Ligang.Wang <wlgchun@163.com>
2019-08-23 23:58:40 +0800

    drop: 删除桥接器的一个code demo

--- avatar-service-dynamic-datasource/src/main/java/com/wlgdo/avatar/service/bridge/UserInterface.java
+++
@@ -1,9 +0,0 @@
-package com.wlgdo.avatar.service.bridge;
-
-/**
- * Author: Ligang.Wang[wlgchun@l63.com]
- * Date: 2019/6/12 23:25
- */
-public interface UserInterface {
-    Object save(Object o);
-}
