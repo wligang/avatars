a5f645e0dfe4f6fa1222ccd388dbcb4e2f455edf
Ligang.Wang <wlgchun@163.com>
2019-10-16 20:00:06 +0800

    添加activiti工作流

--- avatar-activiti/src/main/java/com/wlgdo/avatar/activiti/AvatarActivitiApplication.java
+++ avatar-activiti/src/main/java/com/wlgdo/avatar/activiti/AvatarActivitiApplication.java
@@ -1,19 +1,19 @@
 package com.wlgdo.avatar.activiti;
 
 
-import com.wlgdo.avatar.activiti.listenner.ApplicatonCloseEventListener;
-import com.wlgdo.avatar.activiti.listenner.ApplicatonStartEventListener;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * @author Ligang.Wamg[@foxjk.com]
+ */
 @SpringBootApplication
 public class AvatarActivitiApplication {
 
     public static void main(String[] args) {
         SpringApplication application = new SpringApplication(AvatarActivitiApplication.class);
 
-        application.addListeners(new ApplicatonStartEventListener());
-        application.addListeners(new ApplicatonCloseEventListener());
+
         application.run(args);
     }
 
