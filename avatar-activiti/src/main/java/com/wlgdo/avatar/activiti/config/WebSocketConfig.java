9a0a430bb4ae2139418cda28bb14a8177ef9fe20
Ligang.Wang <wlgchun@163.com>
2019-10-26 23:12:33 +0800

    更新目录

---
+++ avatar-activiti/src/main/java/com/wlgdo/avatar/activiti/config/WebSocketConfig.java
@@ -0,0 +1,75 @@
+package com.wlgdo.avatar.activiti.config;
+
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.config.ChannelRegistration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.RemoteTokenServices;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+/**
+ * @author new
+ * <p>
+ * WebSocket配置类
+ */
+@Slf4j
+@Configuration
+@AllArgsConstructor
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+	private RemoteTokenServices tokenService;
+
+	@Override
+	public void registerStompEndpoints(StompEndpointRegistry registry) {
+		registry.addEndpoint("/ws")
+				.setAllowedOrigins("*")
+				.withSockJS();
+	}
+
+	@Override
+	public void configureMessageBroker(MessageBrokerRegistry registry) {
+		registry.setUserDestinationPrefix("/task/");
+	}
+
+	@Override
+	public void configureClientInboundChannel(ChannelRegistration registration) {
+		registration.interceptors(new ChannelInterceptor() {
+			@Override
+			public Message<?> preSend(Message<?> message, MessageChannel channel) {
+				StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+				// 判断是否首次连接请求
+				if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+					String tokens = accessor.getFirstNativeHeader("Authorization");
+					log.info("webSocket token is {}", tokens);
+					if (StrUtil.isBlank(tokens)) {
+						return null;
+					}
+					// 验证令牌信息
+					OAuth2Authentication auth2Authentication = tokenService.loadAuthentication(tokens.split(" ")[1]);
+					if (ObjectUtil.isNotNull(auth2Authentication)) {
+						SecurityContextHolder.getContext().setAuthentication(auth2Authentication);
+						accessor.setUser(() -> auth2Authentication.getName());
+						return message;
+					} else {
+						return null;
+					}
+				}
+				//不是首次连接，已经成功登陆
+				return message;
+			}
+		});
+	}
+}
