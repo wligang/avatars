0ff3b817a79aa9cdbd10840d03760a4ad38444af
Ligang.Wang <wlgchun@163.com>
2019-11-04 23:54:32 +0800

    modify file

--- avatar-activiti/src/main/java/com/wlgdo/avatar/activiti/listenner/LeaveProcessTaskListener.java
+++ avatar-activiti/src/main/java/com/wlgdo/avatar/activiti/listenner/LeaveProcessTaskListener.java
@@ -1,23 +1,6 @@
-/*
- *    Copyright (c) 2018-2025, lengleng All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions are met:
- *
- * Redistributions of source code must retain the above copyright notice,
- * this list of conditions and the following disclaimer.
- * Redistributions in binary form must reproduce the above copyright
- * notice, this list of conditions and the following disclaimer in the
- * documentation and/or other materials provided with the distribution.
- * Neither the name of the pig4cloud.com developer nor the names of its
- * contributors may be used to endorse or promote products derived from
- * this software without specific prior written permission.
- * Author: lengleng (wangiegie@gmail.com)
- */
-
 package com.wlgdo.avatar.activiti.listenner;
 
-import cn.hutool.core.collection.CollUtil;
+import com.wlgdo.avatar.activiti.util.SpringContextHolder;
 import lombok.extern.slf4j.Slf4j;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.TaskListener;
@@ -25,7 +8,6 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 /**
  * @author Ligang.Wang[@foxjk.com]
@@ -40,25 +22,13 @@ public class LeaveProcessTaskListener implements TaskListener {
      */
     @Override
     public void notify(DelegateTask delegateTask) {
-        delegateTask.setCategory(TaskTypeEnum.LEAVE.getCategory());
-        RemoteUserService userService = SpringContextHolder.getBean(RemoteUserService.class);
-        R<List<SysUser>> result = userService.ancestorUsers(SecurityUtils.getUser().getUsername());
+        delegateTask.setCategory("LEAVE");
         List<String> remindUserList = new ArrayList<>();
-
-        if (CollUtil.isEmpty(result.getData())) {
-            log.info("用户 {} 不存在上级,任务单由当前用户审批", SecurityUtils.getUser().getUsername());
-            delegateTask.addCandidateUser(SecurityUtils.getUser().getUsername());
-            remindUserList.add(SecurityUtils.getUser().getUsername());
-        } else {
-            List<String> userList = result.getData().stream().map(SysUser::getUsername).collect(Collectors.toList());
-            log.info("当前任务 {}，由 {}处理", delegateTask.getId(), userList);
-            delegateTask.addCandidateUsers(userList);
-            delegateTask.setCategory("LeaveBill");
-            remindUserList.addAll(userList);
-        }
+        delegateTask.addCandidateUser("userId-1");
+        remindUserList.add("userId-1");
         SimpMessagingTemplate simpMessagingTemplate = SpringContextHolder.getBean(SimpMessagingTemplate.class);
         remindUserList.forEach(user -> {
-            String target = String.format("%s-%s", SecurityUtils.getUser().getUsername(), TenantContextHolder.getTenantId());
+            String target = String.format("%s-%s", "userId-1", "1");
             simpMessagingTemplate.convertAndSendToUser(target, "/remind", delegateTask.getName());
         });
     }
